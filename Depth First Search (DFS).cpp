9c4e79e442db55c6c37f9bfd0cefc2739b2d90af
Code0-Joker0 <195313911+Code0-Joker0@users.noreply.github.com>
2025-10-30 20:06:19 +0530

    Assignment 7A

---
+++ Depth First Search (DFS).cpp
@@ -0,0 +1,50 @@
+/*Application: Web crawlers use DFS to explore web pages systematically,following links and indexing content for search engines. Write a simple program to index web pages using Depth First Search (DFS). The program should simulate a web graph where pages are represented as nodes and hyperlinks as edges.*/
+#include  <iostream>
+using namespace std;
+void DFS(int p,int n,bool visited[],int** adj){
+    visited[p]=1;
+    cout<<"Visited Page No. "<<p+1<<endl;
+    for(int i=0;i<n;i++){
+        if(adj[p][i]==1 && !visited[i]){
+            DFS(i,n,visited,adj);
+        };
+    };
+};
+int main(){
+    int n;
+    cout<<"Enter Number of Web pages:";
+    cin>>n;
+    bool* visited=new bool[n];
+    int** adj=new int*[n];
+    for (int i=0;i<n;i++){
+        adj[i]=new int[n];
+    };
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                adj[i][j]=0;
+            };
+        };
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            int ch=0;
+            cout<<"If There is Hyperlink from Webpage "<<i+1<<" to "<<j+1<<"\nEnter 1 Else 0:";
+            cin>>ch;
+            if(ch==1){
+                adj[i][j]=ch;
+                adj[j][i]=ch;
+            }
+        };
+        cout<<endl;
+    };
+for (int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            cout<<adj[i][j]<<" ";
+        };
+        cout<<endl;
+    };
+    for(int i=0;i<n;i++){
+        visited[i]=0;
+    };
+    DFS(0,n,visited,adj);
+    return 0;
+};
